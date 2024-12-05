e4aed2688ff108bce6ad86082ea675a4da9c84c5
Nahid Hasan <nahidn228@gmail.com>
2024-12-05 12:03:57 +0600

    DELETE implemented

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -76,13 +76,13 @@ async function run() {
       res.send(result);
     });
 
-    // //DELETE
-    // app.delete("/movies/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await movieCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    //DELETE
+    app.delete("/movies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // //Find
     // app.get("/movies/:id", async (req, res) => {
